743cac748d9baea68c41edd9edfdc28ddd3704b3
Owen <p3r.233@gmail.com>
2014-04-09 01:10:33 -0400

    Now able to build and run outside of eclipse

---
+++ build.sh
@@ -0,0 +1,4 @@
+javac PointMass.java
+javac -cp .:/usr/share/lwjgl/jar/* exec.java
+#jar cf JPlanets.jar exec.java Pointmass.java
+#why (.:)?
---
+++ run.sh
@@ -0,0 +1,1 @@
+java -cp .:/usr/share/lwjgl/jar/* -Djava.library.path=/usr/share/lwjgl/native/linux/ exec
